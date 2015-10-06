d15b8846695a6a612a12bb487b9ee298b75f6c02
Zach Kinstner <zach@aestheticinteractive.com>
2015-10-06 10:02:14 -0400

    Added an "isConnected" condition to the HardwareDetection function.

--- HardwareDetection.cpp
+++ HardwareDetection.cpp
@@ -12,6 +12,13 @@ HardwareDetection::HardwareDetection() : mFound(false) {
 
 /*----------------------------------------------------------------------------------------------------*/
 OSVR_ReturnCode HardwareDetection::operator()(OSVR_PluginRegContext pContext) {
+	Leap::Controller controller;
+
+	if ( !controller.isConnected() ) {
+		mFound = false;
+		return OSVR_RETURN_FAILURE;
+	}
+
 	if ( !mFound ) {
 		mFound = true;
 		osvr::pluginkit::registerObjectForDeletion(pContext, new ControllerDevice(pContext));
