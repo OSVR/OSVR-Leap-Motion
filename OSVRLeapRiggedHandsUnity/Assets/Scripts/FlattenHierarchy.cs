5d243426d944d7ebe5ed04a9c6a50e9262f528ae
Greg Aring <greg@sensics.com>
2017-03-24 16:26:47 -0400

    Added more rigged hands and hand controller.

---
+++ OSVRLeapRiggedHandsUnity/Assets/Scripts/FlattenHierarchy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlattenHierarchy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform c in this.transform)
+        {
+            c.transform.parent = this.transform;
+            foreach(Transform gc in c)
+            {
+                gc.transform.parent = this.transform;
+                foreach (Transform gcc in gc)
+                {
+                    gcc.transform.parent = this.transform;
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
